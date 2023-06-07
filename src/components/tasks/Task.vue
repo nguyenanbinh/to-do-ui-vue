3082404c179c775c8756afe1131009a3198b0379
binh <binh@binh.myweb.real>
2023-06-07 11:29:52 +0700

    display task

---
+++ src/components/tasks/Task.vue
@@ -0,0 +1,31 @@
+<template>
+    <li class="list-group-item py-3">
+        <div class="d-flex justify-content-start align-items-center">
+            <input class="form-check-input mt-0 completed" type="checkbox" />
+            <div class="ms-2 flex-grow-1" title="Double click the text to edit or remove">
+                <!-- <div class="relative">
+                    <input class="editable-task" type="text" />
+                 </div> -->
+                <span>{{ task.name }}</span>
+            </div>
+            <!-- <div class="task-date">24 Feb 12:00</div> -->
+        </div>
+        <div class="task-actions">
+            <button class="btn btn-sm btn-circle btn-outline-secondary me-1">
+                <PencilIcon />
+            </button>
+            <button class="btn btn-sm btn-circle btn-outline-danger">
+                <TrashIcon />
+            </button>
+        </div>
+    </li>
+</template>
+
+<script setup>
+import PencilIcon from '../icons/PencilIcon.vue';
+import TrashIcon from '../icons/TrashIcon.vue';
+
+defineProps({
+    task: Object,
+})
+</script>
---
+++ src/http/task-api.js
@@ -0,0 +1,14 @@
+import api from './api';
+
+const resource = '/tasks';
+
+export const allTasks = () => api.get(resource);
+
+export const createTask = (task) => api.post(resource, task);
+
+export const updateTask = (id, task) => api.put(`${resource}/${id}`, task);
+
+export const deleteTask = (id) => api.delete(`${resource}/${id}`, task);
+
+export const completeTask = (id) => api.put(`${resource}/${id}/complete`, task);
+
