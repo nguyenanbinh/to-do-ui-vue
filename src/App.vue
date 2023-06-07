8df1d9ff9be728edbb331858b9071fc6781fc959
binh <binh@binh.myweb.real>
2023-06-07 09:39:12 +0700

    init to do vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+import TaskPage from './pages/TaskPage.vue';
+</script>
+
+<template>
+<Navbar/>
+<TaskPage/>
+<Footer/>
+</template>
+
+<style scoped>
+
+</style>
